d68bec8fee6d55902af852662fa353d794d48b02
BThanatonz <super.b99@hotmail.com>
2023-11-11 11:07:08 +0700

    update the math library

--- math.js
+++ math.js
@@ -1,15 +1,10 @@
-function sum(a,b){
-    if(Array.isArray(a)){
-        let total = 0;
-        for (let i = 0;i<a.length;i++){
-            total += a[i];
-        }
-        return total;
-    } else if (typeof a === 'number' && typeof b === 'number'){
-        return a + b;
-    
+function sum(a, b) {
+    if (Array.isArray(a)) {
+      return a.reduce((acc, n) => acc + n, 0)
+    } else if (typeof a === 'number' && typeof b === 'number') {
+      return a + b
     }
-}
+  }  
 
 module.exports = {
     sum
